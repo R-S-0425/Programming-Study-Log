62e024cab6a5babb6d5e1656d168dc65b3ea7c63
R-S-0425 <rinrintarou0425@docomo.ne.jp>
2025-12-15 22:44:54 +0900

    Implement swap function using pointers

---
+++ swap.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+void swap(int *x, int *y){
+    int temp = *x;
+    *x = *y;
+    *y = temp;
+}
+
+int main(){
+    int a = 10;
+    int b = 20;
+
+    printf("Before a = %d, b = %d\n", a, b);
+
+    swap(&a,&b);
+
+    printf("After a= %d, b = %d\n", a, b);
+
+    return 0;
+}
